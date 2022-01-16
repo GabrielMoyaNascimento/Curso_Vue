055db19d6534c2fce11195c9157d07831a0beb8d
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-01-15 23:08:55 -0300

    Criando primeiro projeto

---
+++ projeto-01-monstro/app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerLife: 100,
+        monsterLife: 100,
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+
+    },
+    watch: {
+
+    }
+})
