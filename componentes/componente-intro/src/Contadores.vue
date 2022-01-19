d42bae283db32f4cbfb622a6859448db4f8c9d80
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-01-19 17:25:50 -0300

    Estudando componentes

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contadores">
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c"/>
+    <button>?</button>
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+
+export default {
+  components:{ 'app-contador': Contador }
+};
+</script>
+
+<style>
+  
+</style>
