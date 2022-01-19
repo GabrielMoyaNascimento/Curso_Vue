c6d0af0e5b86337574b7ba70e4a465b3ddbde2f9
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-01-19 17:53:57 -0300

    Corrigindo commit do desafio

---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Menu />
+
+    <Toolbar />
+
+    <v-content>
+      <Carrosel />
+    </v-content>
+
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from "@/components/template/Footer.vue";
+import Carrosel from "@/components/widgets/Carrosel.vue";
+import Toolbar from "@/components/template/Toolbar.vue";
+import Menu from "@/components/template/Menu.vue";
+
+export default {
+  components: {
+    Footer,
+    Carrosel,
+    Toolbar,
+    Menu,
+  },
+};
+</script>
