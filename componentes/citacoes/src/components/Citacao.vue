c0cffa164f15b4d7fc222fa2d4ebb1baeda169be
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-01-25 22:48:42 -0300

    Realizando exercício de componentes avançados

---
+++ componentes/citacoes/src/components/Citacao.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="citacao">
+        <slot name="fonte"></slot>
+        <slot></slot>
+        <div class="autor"></div><slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log('created')
+    },
+    destroyed(){
+        console.log('destroyed')
+    },
+    activated(){
+        console.log('activated')
+    },
+    deactivated(){
+        console.log('deactivated')
+    }
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    .autor{
+        float: right;
+    }
+</style>
