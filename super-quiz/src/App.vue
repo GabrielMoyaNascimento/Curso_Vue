48edeb09998278632d546e6aa491e4dbabd37580
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-02-01 15:46:28 -0300

    Finalizado Quiz com animaçoes

--- super-quiz/src/App.vue
+++ super-quiz/src/App.vue
@@ -1,12 +1,44 @@
 <template>
 	<div id="app">
 		<h1>Super Quiz</h1>
+		<transition name="flip" mode="out-in">
+			<Question v-if="questionMode"
+				:question="questions[currentQuestion]"
+				@answered="showResult" />
+			<Result v-else
+				:result="result" 
+				@confirmed="nextQuestion" />
+		</transition>
 	</div>
+
 </template>
 
 <script>
-export default {
+import questions from './util/questions'
+import Question from './components/Question.vue'
+import Result from './components/Result.vue'
 
+export default {
+	components: { Question, Result },
+	data() {
+		return{
+			result: false,
+			questionMode: true,
+			questions,
+			currentQuestion: 0
+		}
+	},
+	methods: {
+		showResult(result){
+			this.result = result
+			this.questionMode = false
+		},
+		nextQuestion(){
+			let r = Math.random() * this.questions.length
+			this.currentQuestion = parseInt(r)
+			this.questionMode = true
+		}
+	}
 }
 </script>
 
